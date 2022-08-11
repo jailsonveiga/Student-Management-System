935e815ce02892b3bdbabb01eb8333c2f32d2b22
jailsonveiga <jmdaveiga2012@gmail.com>
2022-08-10 21:36:43 -0400

    Initial Commit

---
+++ src/test/java/com/jay/sms/StudentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jay.sms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
