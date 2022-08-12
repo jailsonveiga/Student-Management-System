485b732fcc551020908de0bf079ce80a24b7e229
jailsonveiga <jmdaveiga2012@gmail.com>
2022-08-11 22:20:45 -0400

    Implements StudentServiceImpl

--- src/main/java/com/jay/sms/service/impl/StudentServiceImpl.java
+++ src/main/java/com/jay/sms/service/impl/StudentServiceImpl.java
@@ -1,6 +1,23 @@
 package com.jay.sms.service.impl;
 
+import com.jay.sms.entity.Student;
+import com.jay.sms.repository.StudentRepository;
 import com.jay.sms.service.StudentService;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class StudentServiceImpl implements StudentService {
+
+    private StudentRepository studentRepository;
+
+    public StudentServiceImpl(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
 }
